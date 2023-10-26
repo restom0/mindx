3966c583d74a1015d64873dfaea715c05bbb7f14
restom0 <98004370+restom0@users.noreply.github.com>
2023-10-26 09:58:56 +0700

    final_project

--- CIJS87/final-project/src/routes/user.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const UserController = require("../app/controllers/UserController");
-router.get("/", UserController.auth);
-module.exports = router;
